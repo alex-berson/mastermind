f9babfba941725fd4bce2b5d96fa7f6c0b779464
Alexander Berson <aberson@yahoo.com>
2020-12-09 09:00:41 +0100

    initial board design

---
+++ js/mastermind.js
@@ -0,0 +1,18 @@
+
+
+const init = () => {
+
+   
+}
+
+window.onload = () => {
+    document.fonts.ready.then(() => {
+
+        // const preventDefault = (e) => e.preventDefault();
+        
+        // document.body.addEventListener('touchmove', preventDefault, { passive: false });
+        
+        init(); 
+    });
+}
+
